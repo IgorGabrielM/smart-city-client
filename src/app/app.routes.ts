807b44e1aecf2cb92eb31b9b533f84bd3fd827ee
M493750 <@M493750>
2025-05-31 19:02:43 -0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Define a tela inicial como Home
+  // { path: 'about', component: AboutComponent } // Exemplo de outra página
+];
